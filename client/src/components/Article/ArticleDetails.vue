22661e319a3fd784cb82e42a8c29de71a84eda21
Tiago Dias <tiago@epiccode.dev>
2021-05-04 11:05:10 +0100

    Article Details - UI Improvements

--- client/src/components/Article/ArticleDetails.vue
+++ client/src/components/Article/ArticleDetails.vue
@@ -4,26 +4,30 @@
   </div>
   <div v-else class="hello flex-col items-center justify-center container mx-auto mt-10 space-y-10">
     <img class="w-full max-h-96" :src="'https://ipfs.infura.io/ipfs/' + article.imageUrl" alt="Article Banner">
-    <div v-if="!loading">
-      <p class="text-lg">
+    <figcaption class="font-medium">
+      <div class="text-gray-500">
+        Author: <span class="text-blue-500">{{ article.authorName }}</span>
+      </div>
+      <div class="text-gray-500">
+        Published: <span class="text-blue-500">{{ format_time(article.date.toNumber()) }}</span>
+      </div>
+      <div class="text-gray-500">
+        Published: {{ article.published ? "Yes" : "No" }}
+      </div>
+    </figcaption>
+    <div v-if="!loading" class="space-y-2">
+      <p class="text-2xl font-semibold">
         {{ article.title }}
       </p>
-      <p class="text-lg font-semibold">
+      <p class="text-gray-800">
         {{ article.content }}
       </p>
-      <figcaption class="font-medium">
-        <div class="text-blue-500">
-          {{ article.author }}
-        </div>
-        <div class="text-gray-500">
-          {{ article.published ? "Yes" : "No" }}
-        </div>
-      </figcaption>
     </div>
   </div>
 </template>
 
 <script>
+import web3 from 'web3'
 import { mapActions } from "vuex";
 import Article from '../../store/models/Article'
 export default {
@@ -60,10 +64,22 @@ export default {
           json[3],
           json[4],
           json[5],
-          json[6]
+          json[6],
+          json[7]
         ])
 
+      const author = await this.$store.getters.contract.users(this.article.author)
+      this.article.authorName = web3.utils.hexToUtf8(author.name)
+
       this.loading = false
+    },
+    format_time(s) {
+      const dtFormat = new Intl.DateTimeFormat('en-GB', {
+        dateStyle: 'full',
+        timeZone: 'UTC'
+      });
+      
+      return dtFormat.format(new Date(s * 1e3));
     }
   },
 };
