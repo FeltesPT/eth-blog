e3ef2fd7745c68a49b4e7f1e9730462b43acd82b
Tiago Dias <tiago@epiccode.dev>
2021-05-14 11:39:08 +0100

    Backend - Only admin can write articles

--- contracts/Blog.sol
+++ contracts/Blog.sol
@@ -29,6 +29,7 @@ contract Blog {
     uint date;
   }
 
+  address payable owner;
   uint16 public articleCount = 0;
   uint public commentCount = 0;
   Article[] private articles;
@@ -85,12 +86,13 @@ contract Blog {
   );
 
   constructor() {
+    owner = payable(msg.sender);
     createUser("Tiago Dias");
-    createArticle(
-      "My first blog's title.",
-      "QmQpmL2kW6YfTmGwmN82AqNUAh7rbp6rTj3QDBaAsrWgcm",
-      "My first blog article content!"
-    );
+    // createArticle(
+    //   "My first blog's title.",
+    //   "QmQpmL2kW6YfTmGwmN82AqNUAh7rbp6rTj3QDBaAsrWgcm",
+    //   "My first blog article content!"
+    // );
     // createArticle(
     //   "My second blog's title.",
     //   "QmfG4fbLdwbhaS3beJuAhJduhCuvrFd8cqui6EDzVkdGYd",
@@ -105,7 +107,7 @@ contract Blog {
   }
 
   // Article Methods
-  function createArticle(string memory _title, string memory _imageHash, string memory _content) public userExists(msg.sender) {
+  function createArticle(string memory _title, string memory _imageHash, string memory _content) public userExists() isOwner() {
     articles.push(Article(articleCount, block.timestamp, _title, _imageHash, _content, payable(msg.sender), false, 0));
     
     articleToUser[articleCount] = msg.sender;
@@ -117,9 +119,9 @@ contract Blog {
 
   function editArticle(uint16 _id, string memory _title, string memory _imageHash, string memory _content)
     public
-    userExists(msg.sender)
+    userExists()
     articleExists(_id)
-    isOwner(_id) {
+    isAuthor(_id) {
       
       Article storage _article = articles[_id];
       _article.title = _title;
@@ -185,15 +187,15 @@ contract Blog {
       return _myArticles;
   }
 
-  function togglePublished(uint16 _id) external articleExists(_id) isOwner(_id) {
+  function togglePublished(uint16 _id) external articleExists(_id) isAuthor(_id) {
     Article storage _article = articles[_id];
     _article.published = !_article.published;
     _article.date = block.timestamp;
     emit ArticlePublished(_article.id, _article.date, _article.published);
   }
 
-  modifier userExists(address wallet_address) {
-    if (users[wallet_address].name == '') {
+  modifier userExists() {
+    if (users[msg.sender].name == '') {
       revert();
     }
     _;
@@ -206,22 +208,29 @@ contract Blog {
     _;
   }
 
-  modifier isOwner(uint16 _id) {
+  modifier isAuthor(uint16 _id) {
     if (articles[_id].author != msg.sender) {
       revert();
     }
     _;
   }
 
-  modifier isNotOwner(uint16 _id) {
+  modifier isNotAuthor(uint16 _id) {
     if (articles[_id].author == msg.sender) {
       revert();
     }
     _;
   }
 
+  modifier isOwner() {
+    if (owner != msg.sender) {
+      revert();
+    }
+    _;
+  }
+
   // Tips
-  function tipArticle(uint16 _id) public payable articleExists(_id) isNotOwner(_id) {
+  function tipArticle(uint16 _id) public payable articleExists(_id) isNotAuthor(_id) {
     Article memory _article = articles[_id];
 
     address payable _author_address = _article.author;
@@ -240,7 +249,7 @@ contract Blog {
   }
 
   // Comments methods
-  function addCommentToArticle(uint16 _articleId, string memory _message) external articleExists(_articleId) userExists(msg.sender) {
+  function addCommentToArticle(uint16 _articleId, string memory _message) external articleExists(_articleId) userExists() {
     comments.push(Comment(commentCount, _articleId, _message, payable(msg.sender), 0, block.timestamp));
 
     commentToArticle[commentCount] = _articleId;
--- test/Blog.test.js
+++ test/Blog.test.js
@@ -6,7 +6,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('Blog', ([deployer, author1, author2, user1]) => {
+contract('Blog', ([deployer, user1, user2, user3]) => {
 
   before(async () => {
     this.blog = await Blog.deployed()
@@ -22,32 +22,29 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
       assert.notEqual(address, undefined, "[Blog contract address is undefined]")
     })
 
-    it('creates user and one article', async () => {
-      const articleCount = await this.blog.articleCount()
-      assert.equal(articleCount, 1, "[We don't have one Article on the array]")
-
-      const article = await this.blog.getArticle(0)
-      const user = await this.blog.users(article.author);
+    it('creates user', async () => {
+      const user = await this.blog.users(deployer)
       assert.equal(web3.utils.hexToUtf8(user.name), "Tiago Dias", "[User is not correct]")
       assert.equal(user.wallet_address, deployer, "[User is not correct]")
     })
 
   })
 
-  describe('Users', async () => {
-    it('create user', async () => {
-      const result = await this.blog.createUser(web3.utils.stringToHex("Author 1"), { from: author1 })
-      const event = result.logs[0].args
-
-      assert.equal(web3.utils.hexToUtf8(event.name), "Author 1", ['Wrong name on user create'])
-      assert.equal(event.wallet_address, author1, ['Wrong Address on user create'])
-      assert.equal(event.tips_received, 0, ['Wrong Address on user create'])
-      assert.equal(event.tips_sent, 0, ['Wrong Address on user create'])
-    })
+ describe('Users', async () => {
 
     it('Gets user', async () => {
       const user = await this.blog.users(deployer)
       assert.equal(web3.utils.hexToUtf8(user.name), "Tiago Dias", ['Wrong name on user create'])
+    })
+
+    it('create user', async () => {
+      const result = await this.blog.createUser(web3.utils.stringToHex("User 1"), { from: user1 })
+      const event = result.logs[0].args
+
+      assert.equal(web3.utils.hexToUtf8(event.name), "User 1", ['Wrong name on user create'])
+      assert.equal(event.wallet_address, user1, ['Wrong Address on user create'])
+      assert.equal(event.tips_received, 0, ['Wrong Address on user create'])
+      assert.equal(event.tips_sent, 0, ['Wrong Address on user create'])
     })
   })
 
@@ -55,8 +52,25 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
     const imageHash = 'QmQpmL2kW6YfTmGwmN82AqNUAh7rbp6rTj3QDBaAsrWgcm' // Wolf
     const secondImageHash = 'QmfG4fbLdwbhaS3beJuAhJduhCuvrFd8cqui6EDzVkdGYd' // Unicorn
 
+    it('Creates first article', async () => {
+      const result = await this.blog.createArticle("My first blog's title.", imageHash , "My first blog article content!", { from: deployer })      
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), 0, "[Wrong article ID]")
+      assert.notEqual(event.date.toNumber(), null, "[Wrong article date]")
+      assert.equal(event.title, "My first blog's title.", "[Wrong article Title]")
+      assert.equal(event.imageHash, imageHash, "[Wrong article Image Hash]")
+      assert.equal(event.content, "My first blog article content!", "[Wrong article Content]")
+      assert.equal(event.published, false, "[Wrong article Published Status]")
+      assert.equal(event.tips, 0, "[Wrong Tips amoung]")
+
+      const article_user = await this.blog.users(event.author)
+      assert.equal(web3.utils.hexToUtf8(article_user.name), "Tiago Dias", "[Wrong article Author]")
+      assert.equal(article_user.wallet_address, event.author, "[Wrong article Author]")
+      assert.equal(article_user.wallet_address, deployer, "[User is not correct]")
+    })
+
     it('Cant get unpublished article if not author', async () => {
-      await this.blog.getArticle(0, { from: author1 }).should.be.rejected
+      await this.blog.getArticle(0, { from: user1 }).should.be.rejected
     })
 
     it('Can get unpublished article if is author', async () => {
@@ -116,7 +130,7 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
       const article = await this.blog.getArticle(0)
       assert.equal(article.published, false, "[Wrong article Published Status]")
 
-      await this.blog.togglePublished(0, { from: author1 }).should.be.rejected
+      await this.blog.togglePublished(0, { from: user1 }).should.be.rejected
     })
 
     it('toggles article publish', async () => {
@@ -147,7 +161,7 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
       assert.equal(article_user.wallet_address, deployer, "[Wrong article Author]")
     })
 
-    it('creates article', async () => {
+    it('Creates another article', async () => {
       const result = await this.blog.createArticle("A secondary title", imageHash , "A secondary article", { from: deployer })      
       const event = result.logs[0].args
       assert.equal(event.id.toNumber(), 1, "[Wrong article ID]")
@@ -170,7 +184,7 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
       let article = await this.blog.getArticle(0)
       assert.equal(article.tips, 0, "[Article shouldnt have any tips]")
 
-      await this.blog.tipArticle(article.id, { from: author1 , value: tipAmount })
+      await this.blog.tipArticle(article.id, { from: user1 , value: tipAmount })
 
       article = await this.blog.getArticle(0)
       assert.equal(article.tips, tipAmount, "[Should have 0.1 ether]")
@@ -178,7 +192,7 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
       const _deployer = await this.blog.users(deployer)
       assert.equal(web3.utils.BN(_deployer.tips_received), tipAmount, "[Should have 0.1 ether]")
 
-      const _author1 = await this.blog.users(author1)
+      const _author1 = await this.blog.users(user1)
       assert.equal(web3.utils.BN(_author1.tips_sent), tipAmount, "[Should have 0.1 ether]")
     })
 
@@ -189,23 +203,9 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
       await this.blog.tipArticle(article.id, { from: deployer , value: tipAmount }).should.be.rejected
     })
 
-    // Can't create article if user has no name,i.e. is not created
-    it('Cant create article if user has no name', async () => {
-      await this.blog.createArticle("Title", imageHash, "Amazing Content", { from: author2 }).should.be.rejected
-    })
-
-    it('Create article after user is created', async () => {
-      await this.blog.createUser(web3.utils.stringToHex("Author 2"), { from: author2 })
-      const result = await this.blog.createArticle("Title", imageHash, "Amazing Content", { from: author2 })
-      const event = result.logs[0].args
-      
-      assert.equal(event.id.toNumber(), 2, "[Wrong article ID]")
-      assert.notEqual(event.date.toNumber(), null, "[Wrong article date]")
-      assert.equal(event.title, "Title", "[Wrong article Title]")
-      assert.equal(event.imageHash, imageHash, "[Wrong article Image Hash]")
-      assert.equal(event.content, "Amazing Content", "[Wrong article Content]")
-      assert.equal(event.published, false, "[Wrong article Published Status]")
-      assert.equal(event.tips, 0, "[Wrong Tips amoung]")
+    // Can't create article if it's not the owner
+    it('Users cant create article', async () => {
+      await this.blog.createArticle("Title", imageHash, "Amazing Content", { from: user2 }).should.be.rejected
     })
 
     it('Edit article title', async () => {
@@ -257,7 +257,7 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
     it('Wrong author should fail to edit article', async () => {
       const articles = await this.blog.getArticles()
       const article = articles[0]
-      await this.blog.editArticle(article.id, "Totally Different Title", secondImageHash, "Totally Different Content", { from: author2 }).should.be.rejected
+      await this.blog.editArticle(article.id, "Totally Different Title", secondImageHash, "Totally Different Content", { from: user2 }).should.be.rejected
     })
   })
 
@@ -276,29 +276,29 @@ contract('Blog', ([deployer, author1, author2, user1]) => {
     })
 
     it('Comment first article as another user', async () => {
-      const result = await this.blog.addCommentToArticle(0, "This is a basic comment", { from: author1 })
+      const result = await this.blog.addCommentToArticle(0, "This is a basic comment", { from: user1 })
       const event = result.logs[0].args
       
       assert.equal(event.id.toNumber(), 1, "[Wrong Comment ID]")
       assert.equal(event.articleId.toNumber(), 0, "[Wrong Comment's Article ID]")
       assert.equal(event.message, "This is a basic comment", "[Wrong comment message]")
-      assert.equal(event.author, author1, "[Wrong Comment author]")
+      assert.equal(event.author, user1, "[Wrong Comment author]")
       assert.equal(event.tips, 0, "[Wrong Tips amoung]")
       assert.notEqual(event.date.toNumber(), null, "[Comment has no date]")
     })
 
     it('Comment not registered user should fail', async () => {
-      await this.blog.addCommentToArticle(0, "This is a basic comment", { from: user1 }).should.be.rejected
+      await this.blog.addCommentToArticle(0, "This is a basic comment", { from: user3 }).should.be.rejected
     })
 
     it('Get user comments', async () => {
       const deployerComments = await this.blog.getUserComments({from: deployer})
       assert.equal(deployerComments.length, 1, "[deployer should have 1 comment]")
 
-      const authorComments = await this.blog.getUserComments({from: author1})
+      const authorComments = await this.blog.getUserComments({from: user1})
       assert.equal(authorComments.length, 1, "[author1 should have 1 comment]")
 
-      const userComments = await this.blog.getUserComments({from: user1})
+      const userComments = await this.blog.getUserComments({from: user3})
       assert.equal(userComments.length, 0, "[User1 shouldn't have any comments]")
     })
 
