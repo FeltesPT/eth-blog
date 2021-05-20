ad62694bbaa4669492f673d695c379a9584f74c1
Tiago Dias <tiago@epiccode.dev>
2021-05-20 17:36:31 +0100

    Small update

--- client/src/views/User/Create.vue
+++ client/src/views/User/Create.vue
@@ -7,7 +7,7 @@
       <!-- Create User Box -->
       <div class="flex-col container rounded-xl px-6 py-4 bg-white bg-opacity-10 mt-10">
         <h1 class="text-3xl text-white text-center">Create User</h1>
-        <p class="text-gray-200 mt-2 max-w-sm">In order to provide you with a full experience, please tell us your details.</p>
+        <p class="text-gray-200 mt-2 max-w-sm">In order to provide you with a full experience, please tell us your name.</p>
         <div class="flex-col mt-8">
           <input class="border border-white border-opacity-100 rounded-md px-2 w-full h-10 bg-white bg-opacity-10 text-white placeholder-white" type="text" placeholder="Name*" v-model="name">
           <input class="text-white font-bold rounded-md flex items-center justify-center bg-blue-900 px-4 py-2 w-full mt-10" value="Create User" type="submit" @click.prevent="submit">
